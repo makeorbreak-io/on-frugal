a6cfeb4bfd59fb10116a7c979457fd9cd94d0f84
tomislaaaav <jmbotelho95@gmail.com>
2018-04-13 14:43:57 +0100

    update on server

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+// search by location
+
+// search by user name
+
+// search by phone number
+
+// search by email
+
+// search by event name
+
+// search by label
+
+// search by date
+
+
+// register user
+
+// login user
+
+// edit profile
+
+// create event
+
+// edit event
+
+// add user to event
+
+// allow user to event
+
+
 module.exports = router;
