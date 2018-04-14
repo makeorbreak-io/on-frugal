b3daa01bafe9dba12f40b7c77355034aca25533a
tomislaaaav <jmbotelho95@gmail.com>
2018-04-14 15:48:23 +0100

    token and info validated

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./files/onfrugal-1523653506834-firebase-adminsdk-sjuy1-8f163ff365.json');
+const serviceAccount = require('../files/onfrugal-1523653506834-firebase-adminsdk-sjuy1-8f163ff365.json');
 
 function init() {
     admin.initializeApp({
@@ -10,12 +10,25 @@ function init() {
 }
 
 function verifyToken(idToken) {
-    admin.auth().verifyIdToken(idToken).then(
-        (decodedToken) => {
-            var uid = decodedToken.uid;
-        }).catch((err) => {
-            console.log(err);
+    return new Promise((resolve, reject) =>
+        admin.auth().verifyIdToken(idToken).then(
+            (decodedToken) => {
+                var uid = decodedToken.uid;
+                resolve(uid);
+            }).catch((err) => {
+                console.err(err);
+                reject(err);
+            })
+    );
+}
+
+function retrieveInfoByToken(idToken) {
+    return new Promise((resolve, reject) => verifyToken(idToken).then(val => {
+        admin.database().ref('/users/' + val).then((snapshot) => {
+            console.log(snapshot);
+            resolve(snapshot);
         });
+    }));
 }
 
 module.exports = {
