61d10cf325a66318f60696d2be88d655ec7d1689
444Duarte <duartemr.pinto@gmail.com>
2018-04-14 15:00:06 +0100

    removed everything

--- android/OnFrugal/main/src/main/java/com/hackoholics/onfrugal/main/domain/model/Search.kt
+++
@@ -1,5 +0,0 @@
-package com.hackoholics.onfrugal.main.domain.model
-
-class Search(var offers: List<Offer>, var users: List<User>) {
-
-}
--- android/OnFrugal/main/src/main/java/com/hackoholics/onfrugal/main/domain/model/User.kt
+++
@@ -1,5 +0,0 @@
-package com.hackoholics.onfrugal.main.domain.model
-
-class User(var id: String, var name: String, var email: String, var phoneNumber: String) {
-
-}
