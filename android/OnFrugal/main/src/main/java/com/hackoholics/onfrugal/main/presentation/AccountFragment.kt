02fa5e64bd729b9195725fcde5dd8553f3805770
444Duarte <duartemr.pinto@gmail.com>
2018-04-14 14:57:29 +0100

    fixed bug

--- android/OnFrugal/main/src/main/java/com/hackoholics/onfrugal/main/presentation/AccountFragment.kt
+++ android/OnFrugal/main/src/main/java/com/hackoholics/onfrugal/main/presentation/AccountFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.*
+import android.widget.Toast
 import com.hackoholics.onfrugal.main.R
 
 class AccountFragment : Fragment() {
